456626ded8383efbc6c427a0253439e68323cd9c
vskolotov <vskolotov@ya.ru>
2022-05-23 23:32:29 +0500

    Практическое задание 6_

--- client-server_application/common/utils.py
+++ client-server_application/common/utils.py
@@ -1,36 +1,43 @@
 """Утилиты"""
 
 import json
+import sys
 from common.variables import MAX_PACKAGE_LENGTH, ENCODING
 
+sys.path.append('../')
+from errors import IncorrectDataRecivedError, NonDictInputError
+from decos import log
 
+
+@log
 def get_message(client):
     """
-    Утилита приёма и декодирования сообщения.
-    Принимает байты, выдаёт словарь, если принято что-то
-    другое возвращает ValueError (ошибку значения)
+    Утилита приёма и декодирования сообщенияпринимает байты выдаёт словарь,
+    если приняточто-то другое отдаёт ошибку значения
+    :param client:
+    :return:
     """
-
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
     if isinstance(encoded_response, bytes):
         json_response = encoded_response.decode(ENCODING)
-        if isinstance(json_response, str):
-            response = json.loads(json_response)
-            if isinstance(response, dict):
-                return response
-            raise ValueError
-        raise ValueError
-    raise ValueError
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
 
 
+@log
 def send_message(sock, message):
     """
-    Утилита кодирования и отправки сообщения:
-    принимает для отправки словарь, получает из него строку,
-    далее превращает строку в байты и отправляет.
+    Утилита кодирования и отправки сообщения
+    принимает словарь и отправляет его
+    :param sock:
+    :param message:
+    :return:
     """
     if not isinstance(message, dict):
-        raise TypeError
+        raise NonDictInputError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
